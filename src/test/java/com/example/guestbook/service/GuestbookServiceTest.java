babef4486778a47438ddff96f3b63c4b2d1e3b33
LeeJin0527 <ijin17463@gmail.com>
2022-02-11 14:12:02 +0900

    [방명록 수정 이슈 해결]
    serviceImple 의  public void changeTitle(String title){
            this.title = title;
        }
    에 매개변수에 엉뚱한 것을 넣어놔서 수정이 안됐던 것임

--- src/test/java/com/example/guestbook/service/GuestbookServiceTest.java
+++ src/test/java/com/example/guestbook/service/GuestbookServiceTest.java
@@ -42,4 +42,25 @@ public class GuestbookServiceTest {
         System.out.println("---------");
         resultDTO.getPageList().forEach(i -> System.out.println(i));
     }
+
+
+    public void testSearch(){
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .type("tc")
+                .keyword("한글")
+                .build();
+        PageResultDTO<GuestbookDTO, Guestbook>  resultDTO = service.getList(pageRequestDTO);
+        System.out.println("PREV:" + resultDTO.isPrev());
+        System.out.println("NEXT:" + resultDTO.isNext());
+        System.out.println("TOTAL:" + resultDTO.getTotalPage());
+
+        for (GuestbookDTO guestbookDTO:resultDTO.getDtoList()){
+            System.out.println(guestbookDTO);
+        }
+        System.out.println("---------");
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
+
+    }
 }
