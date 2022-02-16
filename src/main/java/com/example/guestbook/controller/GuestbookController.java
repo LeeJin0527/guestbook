67c41d94efb4f76296025ebd783219ba16cccb26
LeeJin0527 <ijin17463@gmail.com>
2022-02-17 00:13:32 +0900

    API추가

--- src/main/java/com/example/guestbook/controller/GuestbookController.java
+++ src/main/java/com/example/guestbook/controller/GuestbookController.java
@@ -3,6 +3,13 @@ package com.example.guestbook.controller;
 import com.example.guestbook.dto.GuestbookDTO;
 import com.example.guestbook.dto.PageRequestDTO;
 import com.example.guestbook.service.GuestbookService;
+
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.PackagePrivate;
 import lombok.extern.log4j.Log4j2;
@@ -14,6 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@Api(tags = {"1.게시물"})
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
@@ -21,22 +29,26 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class GuestbookController {
     private final GuestbookService service;
 
+    @ApiOperation(value = "게시글 목록을 반환하는 메소드 ")
     @GetMapping("/")
     public String index(){
         return "redirect:/guestbook/list";
     }
 
+    @ApiOperation(value = "게시글 목록을 반환하는 메소드")
     @GetMapping("/list")
     public void list(PageRequestDTO pageRequestDTO, Model model){
         model.addAttribute("result", service.getList(pageRequestDTO));
 
     }
 
+    @ApiOperation(value = "글 등록하는 메소드")
     @GetMapping("/register")
     public void register(){
         log.info("register get..");
     }
 
+    @ApiOperation(value = "글 등록후에 이동하는  메소드")
     @PostMapping("/register")
     public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes){
         log.info("dto..." + dto);
@@ -46,6 +58,7 @@ public class GuestbookController {
         return "redirect:/guestbook/list";
     }
 
+    @ApiOperation(value = "글 조회 , 수정페이지 ")
     @GetMapping({"/read", "/modify"})
     public void read( @ModelAttribute("requestDTO") PageRequestDTO requestDTO, long gno, Model model ){
 
@@ -57,6 +70,7 @@ public class GuestbookController {
 
     }
 
+    @ApiOperation(value = "게시글 삭제 결과 반환하는 메소드")
     @PostMapping("/remove")
     public String remove(long gno, RedirectAttributes redirectAttributes){
         log.info("gno: " + gno);
@@ -65,6 +79,7 @@ public class GuestbookController {
         return "redirect:/guestbook/list";
     }
 
+    @ApiOperation(value = "게시글 수정 결과를  반환하는 메소드")
     @PostMapping("/modify")
     public String modify(GuestbookDTO dto,
                          @ModelAttribute("requestDTO") PageRequestDTO requestDTO,
